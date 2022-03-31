a74f35ee486825d09bc87bcd501913e7979d0734
Pam Fernandez <ppossen@gmail.com>
2022-03-31 15:58:06 -0500

    final edits

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_costa
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
-
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    # @TODO: YOUR CODE HERE!
-
+    mars_variable = mongo.db.mars_collection.find_one()
+    
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars=mars_variable)
 
 
 # Route that will trigger the scrape function
@@ -25,13 +25,13 @@ def home():
 def scrape():
 
     # Run the scrape function and save the results to a variable
-    # @TODO: YOUR CODE HERE!
+    mars_data = scrape_mars.scrape_all()
 
     # Update the Mongo database using update and upsert=True
-    # @TODO: YOUR CODE HERE!
+    mongo.db.mars_collection.update_one({}, {"$set": mars_data}, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,16 +1,34 @@
+from cgitb import text
+from inspect import Attribute
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 
-def scrape_info():
-    # Set up Splinter
+def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    news_title, news_teaser = mars_news(browser)
+    img_url_titles = mars_hemis(browser)
+
+    data = {
+        'news_title': news_title,
+        'news_teaser': news_teaser,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'hemispheres': img_url_titles,
+    }
+
+    browser.quit
+    return data
+
+
+def mars_news(browser):
+    # Visit the website
+    url = 'https://redplanetscience.com/'
     browser.visit(url)
 
     time.sleep(1)
@@ -19,27 +37,73 @@ def scrape_info():
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Get the average temps
-    # @TODO: YOUR CODE HERE!
+    # Get the latest Mars news
+    news_title = soup.find('div', class_='content_title').text
+    news_teaser = soup.find('div', class_='article_teaser_body').text
+    return news_title, news_teaser
 
-    # Get the min avg temp
-    # @TODO: YOUR CODE HERE!
 
-    # Get the max avg temp
-    # @TODO: YOUR CODE HERE!
+def featured_image(browser):
+    # Visit the website
+    url = 'https://spaceimages-mars.com/'
+    browser.visit(url)
 
-    # BONUS: Find the src for the sloth image
-    # @TODO: YOUR CODE HERE!
+    # Get the featured image
+    full_image = browser.find_by_tag('button')[1]
+    full_image.click()
 
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
 
-    # Quit the browser after scraping
-    browser.quit()
+    try:
+        img_url = soup.find('img', class_="fancybox-image").get('src')
+    except AttributeError:
+        return None
 
-    # Return results
-    return costa_data
+    feat_img_url = f'https://spaceimages-mars.com/{img_url}'
+    return feat_img_url
+
+
+def mars_facts():
+    # Get the Mars facts table
+    try:
+        df = tables = pd.read_html('https://galaxyfacts-mars.com/')[0]
+    except BaseException: 
+        return None
+    
+    df.columns = ['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+
+    return df.to_html()
+
+
+def mars_hemis(browser):
+    # Visit the website
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # Get the Mars hemipshere pictures and their names
+    hemisphere_image_urls = []
+
+    for hemis in range(4):
+        browser.links.find_by_partial_text('Hemisphere')[hemis].click()
+        
+        # Scrape page into Soup
+        html = browser.html
+        hemi_soup = bs(html, 'html.parser')
+
+        title = hemi_soup.find('h2', class_ = 'title').text
+        img_url = hemi_soup.find('li').a.get('href')
+        
+        hemispheres = {}
+        hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    
+    return hemisphere_image_urls
+
+    
+if __name__ == "__main__":
+    print(scrape_all())
